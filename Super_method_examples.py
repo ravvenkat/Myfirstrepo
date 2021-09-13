56625c817471bd52a6ff26c79e132974347bc859
ravvenkat <87017864+ravvenkat@users.noreply.github.com>
2021-09-13 15:42:28 +0530

    Create Super_method_examples.py

---
+++ Super_method_examples.py
@@ -0,0 +1,44 @@
+class Animal:
+  def __init__(self, Animal):
+    print(Animal, 'is an animal.');
+
+class Mammal(Animal):
+  def __init__(self, mammalName):
+    print(mammalName, 'is a warm-blooded animal.')
+    super().__init__(mammalName)
+    
+class NonWingedMammal(Mammal):
+  def __init__(self, NonWingedMammal):
+    print(NonWingedMammal, "can't fly.")
+    super().__init__(NonWingedMammal)
+
+class NonMarineMammal(Mammal):
+  def __init__(self, NonMarineMammal):
+    print(NonMarineMammal, "can't swim.")
+    super().__init__(NonMarineMammal)
+
+class Dog(NonMarineMammal, NonWingedMammal):
+  def __init__(self):
+    print('Dog has 4 legs.');
+    super().__init__('Dog')
+    
+d = Dog()
+print('')
+bat = NonMarineMammal('Bat')
+
+
+
+
+
+
+########################### Output ###########################
+
+Dog has 4 legs.
+Dog can't swim.
+Dog can't fly.
+Dog is a warm-blooded animal.
+Dog is an animal.
+
+Bat can't swim.
+Bat is a warm-blooded animal.
+Bat is an animal.
